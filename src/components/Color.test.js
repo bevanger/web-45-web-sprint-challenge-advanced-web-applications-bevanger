aa2cfdd7cf5196312058bbbc217c86f8b9bf1ad9
bevanger <baileyevanger@gmail.com>
2021-08-20 16:16:12 -0600

    added to testing

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -5,13 +5,28 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+const testColor = { 
+    name: '',
+    color: 'fakeColor',
+    code: {
+        hex:''
+    }
+}
+
 test("Renders without errors with blank color passed into component", () => {
+    render(<Color color={testColor} />)
 });
   
 test("Renders the color passed into component", () => {
+    render(<Color color={testColor}/>)
+
+    const renderedColor = screen.getByText(/fakecolor/i);
+
+    expect(renderedColor).toBeInTheDocument();
 });
 
 test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
